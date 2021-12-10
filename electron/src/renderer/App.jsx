49db6f7e6d6de4de32c2cb584f5437d0dfff07d0
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2021-12-10 10:52:56 -0500

    reconcile rebase issues
    fix the issues that came up rebasing previous tests using JS instead of JS with current changes on the main `electron` branch.

--- electron/src/renderer/App.jsx
+++ electron/src/renderer/App.jsx
@@ -1,67 +1,117 @@
-import { useEffect } from 'react';
-import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-import ObjectId from 'bson-objectid';
-import icon from '../../assets/icon.svg';
+import { useState, useEffect, useContext } from 'react';
+import {
+  MemoryRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation,
+} from 'react-router-dom';
+import { AppBar, Toolbar, Button, Typography } from '@material-ui/core';
 import './App.css';
-import { openRealm, realmApp } from './realm';
+import { RealmAppProvider } from './components/RealmApp';
+import { appId } from '../realm.json';
+import Context from './components/Context';
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
+import Todo from './components/Todo';
+import Welcome from './components/Welcome';
+import useRealmApp from './hooks/useRealmApp';
+import { WelcomePage } from './components/WelcomePage';
 
-const Hello = () => {
-  useEffect(() => {
-    (async () => {
-      const realm = await openRealm();
-      realm?.write(() => {
-        realm.create('Car', {
-          make: 'Toyota',
-          model: 'Prius',
-          miles: 45,
-          _partition: realmApp.currentUser?.id,
-          _id: new ObjectId(),
-        });
-      });
-    })();
-  }, []);
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs!!!
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
+const PrivateRoute = (props) => {
+  const location = useLocation();
+  const { currentUser } = useRealmApp();
+
+  return currentUser?.isLoggedIn ? (
+    <Route {...props} />
+  ) : (
+    <Redirect
+      to={{
+        pathname: '/',
+        state: { from: location },
+      }}
+    />
   );
 };
 
-export default function App() {
+function AppName() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Hello} />
-      </Switch>
-    </Router>
+    <Typography className="app-bar-title" component="h1" variant="h5">
+      Realm Electron Template
+    </Typography>
   );
 }
+
+function FooterInfo() {
+  return (
+    <div className="footer-info">
+      <span>Built with the MongoDB Realm Electron Template</span> |{' '}
+      <Link target="_blank" href="https://docs.mongodb.com/realm">
+        Docs
+      </Link>
+    </div>
+  );
+}
+
+const App = () => {
+  const history = useHistory();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { logOut, currentUser } = useRealmApp();
+
+  useEffect(() => {
+    currentUser?.isLoggedIn === true
+      ? setIsLoggedIn(true)
+      : setIsLoggedIn(false);
+  }, [currentUser?.isLoggedIn]);
+
+  useEffect(() => {
+    if (currentUser?.isLoggedIn) {
+      history.push('/todo');
+    }
+  }, [currentUser?.isLoggedIn]);
+
+  const onLogOut = () => {
+    logOut();
+  };
+
+  return (
+    <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <AppName />
+          {currentUser?.isLoggedIn ? (
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={async () => {
+                await logOut();
+              }}
+            >
+              <Typography variant="button">Log Out</Typography>
+            </Button>
+          ) : null}
+        </Toolbar>
+      </AppBar>
+
+      <Switch>
+        <Route path="/" component={WelcomePage} exact />
+        <PrivateRoute path="/todo" component={Todo} />
+      </Switch>
+    </>
+  );
+};
+
+// defining Router in a separate component so that the App component
+// has access to the `useHistory` hook.
+// See https://stackoverflow.com/a/68173854/17093063
+const AppWithProvider = () => (
+  <RealmAppProvider appId={appId}>
+    <Router>
+      <App />
+      <FooterInfo />
+    </Router>
+  </RealmAppProvider>
+);
+export default AppWithProvider;
--- electron/src/renderer/realm/index.js
+++
@@ -1,5 +0,0 @@
-import Authentication from './Authentication';
-import openRealm from './openRealm';
-import realmApp from './realmApp';
-
-export { Authentication, openRealm, realmApp };
